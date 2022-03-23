8be142d6c2f558ccb3784261ec97dc295fd82c51
intuitiveen <intuitiveen@gmail.com>
2022-03-23 16:48:32 -0400

    switched from web scraping to api use & color changes & overall better code structure

--- index.py
+++ index.py
@@ -1,48 +1,40 @@
-import requests, httpx, ctypes
-from bs4 import BeautifulSoup
+import httpx, ctypes, math
 from colorama import Fore as F, init
-from collections import OrderedDict
 
 init()
 ctypes.windll.kernel32.SetConsoleTitleW(f'ykcehc v0.1 Discord Invite Scrapper')
 
 def banner():
-    print(
-        F.LIGHTRED_EX
-        + """
-    ██████╗ ███████╗██████╗  █████╗ ██████╗  ██████╗███████╗
-    ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝
-    ██████╔╝█████╗  ██████╔╝███████║██████╔╝██║     ███████╗
-    ██╔══██╗██╔══╝  ██╔═══╝ ██╔══██║██╔══██╗██║     ╚════██║
-    ██║  ██║███████╗██║     ██║  ██║██║  ██║╚██████╗███████║
-    ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝
-       
-                Discord Invite Scrapper
-              https://discord.gg4rHg2pyHjF
-"""     
-        + F.RESET)
+    print(F.LIGHTBLUE_EX + """
+██████╗ ███████╗██████╗  █████╗ ██████╗  ██████╗███████╗
+██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝
+██████╔╝█████╗  ██████╔╝███████║██████╔╝██║     ███████╗
+██╔══██╗██╔══╝  ██╔═══╝ ██╔══██║██╔══██╗██║     ╚════██║
+██║  ██║███████╗██║     ██║  ██║██║  ██║╚██████╗███████║
+╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝""" + F.LIGHTYELLOW_EX + """\n
+            Discord Invite Scrapper
+         https://discord.gg4rHg2pyHjF""" + F.RESET)
 
+def topgg_scraper(amount):
+    servers = httpx.get(
+        f"https://top.gg/api/client/entities/search?platform=discord&entityType=server&amount={amount}&nsfwLevel=1&sort=top"
+    ).json()["results"]
+
+    for server in servers:
+        id = server["id"]
+        server = httpx.get(f"https://top.gg/servers/{id}/join", follow_redirects=True)
+        print('\033[01m' + F.LIGHTGREEN_EX + '[✓] ' + F.LIGHTYELLOW_EX + str(server.url) + F.RESET)
+        write(str(server.url) + "\n")
+
+def write(str):
+    with open("servers.txt", "a") as f:
+        f.write(str)
+
+print("✓")
 banner()
 
-servers = []; counter = 1
-
-server_amount = int(input("How many servers would you like to scrape? "))
-print(f"Scraping... This will take about {server_amount} seconds")
-
-while len(servers) < server_amount:
-    url = f"https://top.gg/servers/list/new?page={counter}"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = list(OrderedDict.fromkeys(soup.find_all("a", class_ = "chakra-link")))
-
-    for a in results:
-        if a["href"].endswith("/join"):
-            server = str(httpx.get(f"https://top.gg{a['href']}", follow_redirects = True).url)
-            servers.append(server)
-    
-    counter += 1
-
-with open("servers.txt", "w") as f:
-        f.write("\n".join(servers[0:server_amount]))
-
+final_servers = []
+server_amount = int(input(F.LIGHTBLUE_EX + "How many servers would you like to scrape? " + F.RESET))
+print(f"Scraping...")
+topgg_scraper(int(server_amount))
 print("Done!")
