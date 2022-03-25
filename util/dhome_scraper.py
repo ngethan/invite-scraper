2cbc406251264faa87eb7f6d7d95fdb939bc6ba6
intuitiveen <intuitiveen@gmail.com>
2022-03-24 22:44:37 -0400

    discordhome scraper & write fixes

---
+++ util/dhome_scraper.py
@@ -0,0 +1,19 @@
+import requests, httpx, math
+from bs4 import BeautifulSoup
+from colorama import Fore as F, init; init()
+from util.write import write
+
+def dhome_scraper(amount, query=""):
+    counter = 0
+    for i in range(math.ceil(amount/24)):
+        url = f"https://discordhome.com/?order=voted&page={i}" if not query else f"https://discordhome.com/servers/search?q={query}&o=top-voted&page={i}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find_all("a", class_ = "join")
+        
+        for a in results:
+            if counter == amount: break
+            server = httpx.get(f"https://discordhome.com{a['href']}", follow_redirects = True)
+            print('\033[01m' + F.LIGHTGREEN_EX + '[✓] ' + F.LIGHTMAGENTA_EX + str(server.url) + F.RESET)
+            write(str(server.url) + "\n")
+            counter += 1
--- util/write.py
+++ util/write.py
@@ -1,5 +1,3 @@
-import os; os.chdir("..")
-
 def write(str):
     with open("invites.txt", "a") as f:
         f.write(str)
